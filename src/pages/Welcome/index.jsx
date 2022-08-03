1b2dc9bfcdd48bebfb6a3e280192df9bed6ff8d6
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-08-03 10:13:37 -0300

    Aplicando rotas

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -3,22 +3,32 @@ import AnimatedLottieView from "lottie-react-native";
 import pokemonAnimation from './pokemon.json'
 import * as S from "./styles"
 import { Button } from "react-native";
+import { Platform } from 'react-native'
+import { useNavigation } from "@react-navigation/native";
 
 export function Welcome() {
+  const navigation = useNavigation();
+
+  const handlePress = () => {
+    navigation.navigate('Home');
+  }
+
   return (
     <S.Container>
       <S.Content>
-        <S.WapperAnimation>
-          <S.wrapperImage>
-            <AnimatedLottieView style={{ width: 200 }} autoPlay source={pokemonAnimation} loop />
-          </S.wrapperImage>
-        </S.WapperAnimation>
+        {Platform.OS !== 'web' &&
+          <S.WapperAnimation>
+            <S.wrapperImage>
+              <AnimatedLottieView style={{ width: 200 }} autoPlay source={pokemonAnimation} loop />
+            </S.wrapperImage>
+          </S.WapperAnimation>
+        }
 
         <S.Title>Bem vindo</S.Title>
         <S.SubTitle>Encontro todos os pokemons em apenas um local</S.SubTitle>
       </S.Content>
       <S.Footer>
-        <Button title='Iniciar ' />
+        <Button title='Iniciar 'onPress={handlePress}/>
       </S.Footer>
     </S.Container>
   )
