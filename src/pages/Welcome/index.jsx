f373ce3ecd9cb73e3436a7694f4f075544680730
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-08-03 10:01:42 -0300

    Importando styles e button

--- src/pages/Welcome/index.jsx
+++ src/pages/Welcome/index.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import AnimatedLottieView from "lottie-react-native";
 import pokemonAnimation from './pokemon.json'
-
+import * as S from "./styles"
+import { Button } from "react-native";
 
 export function Welcome() {
-    return <S.Container>
-    <S.Content>
+  return (
+    <S.Container>
+      <S.Content>
         <S.WapperAnimation>
-            <S.wrapperImage>
-                <AnimatedLottieView style ={{width: 200}} autoPlay source={pokemonAnimation} loop />
-            </S.wrapperImage>   
+          <S.wrapperImage>
+            <AnimatedLottieView style={{ width: 200 }} autoPlay source={pokemonAnimation} loop />
+          </S.wrapperImage>
         </S.WapperAnimation>
-        
+
         <S.Title>Bem vindo</S.Title>
         <S.SubTitle>Encontro todos os pokemons em apenas um local</S.SubTitle>
-    </S.Content>
-        <S.Footer>
-            <Button title= 'Iniciar '/>
-        </S.Footer>
-
+      </S.Content>
+      <S.Footer>
+        <Button title='Iniciar ' />
+      </S.Footer>
     </S.Container>
+  )
 }
