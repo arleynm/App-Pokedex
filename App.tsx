485e7889fb5b03673e6cc45ac91b25be60c65a64
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-08-03 10:01:11 -0300

    Buscando o Ptovider do styled components native

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components/native';
 
 import { Routes } from './src/routes/';
 
 import theme from './src/global/styles/theme';
 
 export default function App() {
+  console.log(theme)
   return (
     <ThemeProvider theme={theme}>
       <StatusBar backgroundColor="transparent" translucent />
