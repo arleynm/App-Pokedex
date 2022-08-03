b747828573f735a85f73334c45aaa41fbc46cba1
Ítalo Médici <59889993+ItaloMedici@users.noreply.github.com>
2022-08-03 10:21:31 -0300

    Adicionando React e removendo console.log

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-
 import { ThemeProvider } from 'styled-components/native';
-
 import { Routes } from './src/routes/';
-
 import theme from './src/global/styles/theme';
 
 export default function App() {
-  console.log(theme)
   return (
     <ThemeProvider theme={theme}>
       <StatusBar backgroundColor="transparent" translucent />
